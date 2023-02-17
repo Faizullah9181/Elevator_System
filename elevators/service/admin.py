d4d7e0a481915d9088d3058d55667e1abdfc12bb
FAIZULLAH <faizullah9181@gmail.com>
2023-02-18 00:07:58 +0530

    models design and some changes

--- elevators/service/admin.py
+++ elevators/service/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Building, Elevator, ElevatorManager
 
 # Register your models here.
+
+admin.site.register(Building)
+admin.site.register(Elevator)
+admin.site.register(ElevatorManager)
--- elevators/service/models.py
+++ elevators/service/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class ElevatorManager(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+
+class Building(models.Model):
+    floors = models.IntegerField(default=10)
+    elevators = models.IntegerField(default=1)
+
+
+class Elevator(models.Model):
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
+    current_floor = models.IntegerField(default=1)
+    is_moving = models.BooleanField(default=False)
+    is_door_open = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_reached_top = models.BooleanField(default=False)
+    is_reached_bottom = models.BooleanField(default=False)
+    requests = models.ManyToManyField(User , related_name='requests', blank=True)
