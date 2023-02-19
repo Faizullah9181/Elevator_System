9ae090ce06d96e85f6461cb57157cbd2f5a26c86
FAIZULLAH <faizullah9181@gmail.com>
2023-02-19 13:28:34 +0530

    db

--- elevators/elevators/settings.py
+++ elevators/elevators/settings.py
@@ -120,8 +120,12 @@ WSGI_APPLICATION = 'elevators.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'Ltlp13oWU9kQYOf9Y2XB',
+        'HOST': 'containers-us-west-59.railway.app',
+        'PORT': '5467',
     }
 }
 
